966209d1c064b217cb80133db14ae42d331cd4f8
Techatrix <techatrix@mailbox.org>
2025-05-19 05:20:06 +0200

    fix assumptions about how fd_prestat_get may be called (microsoft/vscode-wasm#226)
    The current implementation of `fd_prestat_get` does not appears to handle the following cases properly:
    
    - fd_prestat_get is called on the same file descriptor again
    - fd_prestat_get is called on stdio file descriptors (fd < 3)
    - fd_prestat_get is called in non sequential order of file descriptors

--- wasm-wasi-core/src/common/service.ts
+++ wasm-wasi-core/src/common/service.ts
@@ -195,10 +195,12 @@ export interface EnvironmentOptions extends Omit<ProcessOptions, 'args' | 'trace
 }
 
 export namespace EnvironmentWasiService {
-	export function create(fileDescriptors: FileDescriptors, programName: string, preStats: IterableIterator<[string, DeviceDriver]>, options: EnvironmentOptions): EnvironmentWasiService {
+	export function create(fileDescriptors: FileDescriptors, programName: string, preStats: [string, DeviceDriver][], options: EnvironmentOptions): EnvironmentWasiService {
 
 		const $encoder: RAL.TextEncoder = RAL().TextEncoder.create(options?.encoding);
 		const $preStatDirnames: Map<fd, string> = new Map();
+
+		fileDescriptors.switchToRunning(3 + preStats.length);
 
 		const result: EnvironmentWasiService = {
 			args_sizes_get: (memory: ArrayBuffer, argvCount_ptr: ptr<u32>, argvBufSize_ptr: ptr<u32>): Promise<errno> => {
@@ -266,32 +268,19 @@ export namespace EnvironmentWasiService {
 			},
 			fd_prestat_get: async (memory: ArrayBuffer, fd: fd, bufPtr: ptr<prestat>): Promise<errno> => {
 				try {
-					// FIXME: current impl is broken; cannot be called out-of-order or after program init
-					// this workaround remedies it partially with a simple upsert strategy
-					async function lookupMountPoint(fd: fd) {
-						if ($preStatDirnames.has(fd)) {
-							return $preStatDirnames.get(fd)!;
-						}
-
-						// still, this cannot be called out-of-order
-						const next = preStats.next();
-						if (next.done === true) {
-							// MUST throw EBADF
-							try {
-							  fileDescriptors.switchToRunning(fd);
-							} catch (_e) {}
-							throw new WasiError(Errno.badf);
-						}
-
-						const [ mountPoint, driver ] = next.value;
+					if (fd < 3) {
+						return Errno.badf;
+					}
+					if (fd - 3 >= preStats.length) {
+						return Errno.badf;
+					}
+					const [ mountPoint, driver ] = preStats[fd - 3];
+					if (!fileDescriptors.has(fd)) {
 						const fileDescriptor = await driver.fd_create_prestat_fd(fd);
 						fileDescriptors.add(fileDescriptor);
 						fileDescriptors.setRoot(driver, fileDescriptor);
 						$preStatDirnames.set(fileDescriptor.fd, mountPoint);
-						return mountPoint;
 					}
-
-					const mountPoint = await lookupMountPoint(fd);
 					const view = new DataView(memory);
 					const prestat = Prestat.create(view, bufPtr);
 					prestat.preopentype = Preopentype.dir;
@@ -1188,7 +1177,7 @@ export namespace FileSystemService {
 		const clock = Clock.create();
 		return Object.assign(
 			{},
-			EnvironmentWasiService.create(fileDescriptors, 'virtualRootFileSystem', preOpens.entries(), {}),
+			EnvironmentWasiService.create(fileDescriptors, 'virtualRootFileSystem', Array.from(preOpens.entries()), {}),
 			DeviceWasiService.create(deviceDrivers, fileDescriptors, clock, virtualRootFileSystem, options)
 		);
 	}
