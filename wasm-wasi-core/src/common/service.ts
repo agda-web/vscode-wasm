6e03e9262ee4cffa34cf33417d142da18355eca3
Andy Pan <andy0130tw@yahoo.com.tw>
2025-08-01 02:28:31 +0800

    Check and record if the last preopen fd past one has been probed

--- wasm-wasi-core/src/common/service.ts
+++ wasm-wasi-core/src/common/service.ts
@@ -199,6 +199,7 @@ export namespace EnvironmentWasiService {
 
 		const $encoder: RAL.TextEncoder = RAL().TextEncoder.create(options?.encoding);
 		const $preStatDirnames: Map<fd, string> = new Map();
+		let hasProbedPreopenFdPastOne = false;
 
 		const result: EnvironmentWasiService = {
 			args_sizes_get: (memory: ArrayBuffer, argvCount_ptr: ptr<u32>, argvBufSize_ptr: ptr<u32>): Promise<errno> => {
@@ -270,7 +271,8 @@ export namespace EnvironmentWasiService {
 						return Errno.badf;
 					}
 					if (fd - 3 >= preStats.length) {
-						if (fd - 3 === preStats.length) {
+						if (!hasProbedPreopenFdPastOne && fd - 3 === preStats.length) {
+							hasProbedPreopenFdPastOne = true;
 							fileDescriptors.switchToRunning(fd);
 						}
 						return Errno.badf;
